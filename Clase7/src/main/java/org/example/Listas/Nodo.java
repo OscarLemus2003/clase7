a8d52610296bbaa829b184e0dc37f77d362d3b96
OscarLemus2003 <99754052+OscarLemus2003@users.noreply.github.com>
2024-04-08 20:45:03 -0600

    Add files via upload

---
+++ Clase7/src/main/java/org/example/Listas/Nodo.java
@@ -0,0 +1,29 @@
+package org.example.Listas;
+
+public class Nodo {
+    int dato;
+    Nodo enlace;
+    public Nodo(int x){
+        dato = x;
+        enlace = null;
+    }
+    public Nodo (int x, Nodo n){
+        dato = x;
+        enlace = n;
+    }
+    public Nodo getEnlace(){
+        return enlace;
+
+    }
+    public void setEnlace(Nodo n){
+        enlace = n;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "dato=" + dato +
+                ", enlace=" + enlace +
+                '}';
+    }
+}
